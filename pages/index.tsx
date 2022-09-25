3397e1a1aa5f26cfe25fd5c43b7c0582459380e7
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2022-09-25 01:22:19 -0500

    Get data on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,10 @@ import Head from "next/head"
 import Hero from "../components/Hero"
 import Navbar from "../components/Navbar"
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ data }: any) => {
+    console.log(data)
     const bg = useColorModeValue("#fff", "#1A202C")
+
     return (
         <div style={{ background: bg }}>
             <Head>
@@ -19,3 +21,12 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export async function getServerSideProps() {
+    const res = await fetch("http://localhost:3000/api/data")
+    const data = await res.json()
+
+    return {
+        props: { data: data }, // will be passed to the page component as props
+    }
+}
