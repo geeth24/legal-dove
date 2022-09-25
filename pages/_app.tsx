8fcce45e2b41b076a7a675f5cb54a6f0ae7b0166
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2022-09-24 21:58:02 -0500

    9:57 PM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-import Navbar from '../components/Navbar'
+
 import { theme } from '../theme'
 function MyApp({ Component, pageProps }: AppProps) {
   return(
     <ChakraProvider theme={theme}>
-      <Navbar />
       <Component {...pageProps} />
     </ChakraProvider>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next"
+import Head from "next/head"
+import Hero from "../components/Hero"
+import Navbar from "../components/Navbar"
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Pro Bono</title>
-        <link rel="icon" href="/logo.svg"></link>
-      </Head>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>Legal Dove</title>
+                <link rel="icon" href="/logo.svg"></link>
+            </Head>
+            <Navbar />
+            <Hero />
+        </>
+    )
 }
 
 export default Home
