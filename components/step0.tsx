66434409d41d1a88f3f47f17d9befd8758c79a76
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2022-09-25 01:11:34 -0500

    error for step 0 (doesn't work)

--- components/step0.tsx
+++ components/step0.tsx
@@ -4,8 +4,11 @@ import {
     Input,
     InputGroup,
     InputRightElement,
+    FormControl,
+    FormHelperText,
+    FormErrorMessage,
 } from "@chakra-ui/react"
-import React from "react"
+import React, { useState } from "react"
 import { BsArrowRight } from "react-icons/bs"
 
 interface step0Props {
@@ -21,34 +24,45 @@ interface step0Props {
 
 export default function Step0({ setStep, filters }: step0Props) {
     const onClick = () => {
-        setStep(1)
+        setIsError(filters.location === "")
+        if (isError) {
+            setStep(1)
+        }
     }
+    const [isError, setIsError] = useState(false)
 
     return (
         <HStack spacing="24px">
-            <InputGroup>
-                <Input
-                    placeholder="Enter city, state, or zip code"
-                    _placeholder={{ opacity: 0.4, color: "inherit" }}
-                    borderColor="#091D36"
-                    color="#091D36"
-                    _hover={{ borderColor: "#091D36" }}
-                    width="300px"
-                    onChange={(e) => {
-                        filters.location = e.target.value
-                    }}
-                />
-                <InputRightElement width="3.5rem">
-                    <Button
-                        colorScheme="brand"
-                        onClick={onClick}
-                        h="1.75rem"
-                        size="sm"
-                    >
-                        <BsArrowRight />
-                    </Button>
-                </InputRightElement>
-            </InputGroup>
+            <FormControl isInvalid={isError}>
+                <InputGroup>
+                    <Input
+                        placeholder="Enter city, state, or zip code"
+                        _placeholder={{ opacity: 0.4, color: "inherit" }}
+                        borderColor="#091D36"
+                        color="#091D36"
+                        _hover={{ borderColor: "#091D36" }}
+                        width="300px"
+                        onChange={(e) => {
+                            filters.location = e.target.value
+                        }}
+                    />
+                    <InputRightElement width="3.5rem">
+                        <Button
+                            colorScheme="brand"
+                            onClick={onClick}
+                            h="1.75rem"
+                            size="sm"
+                        >
+                            <BsArrowRight />
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+                {!isError ? (
+                    <FormHelperText>Enter the location.</FormHelperText>
+                ) : (
+                    <FormErrorMessage>Location is required.</FormErrorMessage>
+                )}
+            </FormControl>
         </HStack>
     )
 }
