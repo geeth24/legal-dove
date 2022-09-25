a8a2d1b787f1d91abde0e3c08146ab2d1c296eeb
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2022-09-25 00:51:23 -0500

    step3

--- components/step3.tsx
+++ components/step3.tsx
@@ -20,12 +20,17 @@ export default function Step3({ setStep, filters }: step3Props) {
 
     return (
         <HStack spacing="24px">
-            <Select placeholder="Select area of law">
+            <Select
+                placeholder="Select area of law"
+                onChange={(e) => {
+                    filters.area = e.target.value
+                }}
+            >
                 <option value="option1">Option 1</option>
                 <option value="option2">Option 2</option>
                 <option value="option3">Option 3</option>
             </Select>
-            <Button colorScheme="brand" variant="solid">
+            <Button colorScheme="brand" variant="solid" onClick={onClick}>
                 <BsArrowRight />
             </Button>
         </HStack>
